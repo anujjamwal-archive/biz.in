808c50fbff64b7880bf77dc55a8607c8d3709016
Anuj Jamwal <anuj@anuj-VirtualBox.(none)>
2013-12-15 22:48:07 +0530

    Initial project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BizIn::Application.config.secret_key_base = 'dcf4b74d9dce6a86ee2971b57ef9ae53cc7169dc1d5473a26070b8a0460f3e8f71e62f379948761d15ba98d5d0cef1b3700667df97562cb75b2ff2e56340989a'
